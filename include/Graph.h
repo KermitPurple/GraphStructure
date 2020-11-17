5c23a0c4342616f5465491cc16952a3793bf553a
Shane McDonough <shanemcdman@gmail.com>
2020-11-17 01:55:21 -0800

    Create add_edge and add_vertex

--- include/Graph.h
+++ include/Graph.h
@@ -1,16 +1,19 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include<iostream>
 #include<utility>
-#include<list>
+#include<set>
 
 
 class Graph{
 private:
     typedef std::pair<int, int> Edge;
-    std::list<int> Vertices;
-    std::list<int> Edges;
+    std::set<int> vertices;
+    std::set<Edge> edges;
 public:
+    void add_vertex(int vertex);
+    void add_edge(int v1, int v2);
     Graph();
 };
 
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -2,3 +2,18 @@
 
 Graph::Graph(){
 }
+
+void Graph::add_vertex(int vertex){
+    vertices.insert(vertex);
+}
+
+void Graph::add_edge(int v1, int v2){
+    if(!vertices.count(v1) || !vertices.count(v2))
+        return;
+    Edge edge;
+    if(v1 < v2)
+        edge = {v1, v2};
+    else
+        edge = {v2, v1};
+    edges.insert(edge);
+}
