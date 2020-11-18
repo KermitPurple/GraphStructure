68cc797ac1b5375127f374425bc989ac8d149cf3
Shane McDonough <shanemcdman@gmail.com>
2020-11-17 19:40:44 -0800

    Use comments that obey standards

--- include/Graph.h
+++ include/Graph.h
@@ -8,15 +8,11 @@
 
 class Graph{ // unweighted, Unordered, graph data structure
 private:
-    std::map<int, std::set<int>> m; // data in graph\
-    The key corresponds to the vertex\
-    the value is a set of all the vertices that vertex is connected to by edges
+    std::map<int, std::set<int>> m; // data in graph; The key corresponds to the vertex; the value is a set of all the vertices that vertex is connected to by edges
 public:
     void add_vertex(int vertex); // Adds a vertex
-    void add_edge(int v1, int v2); // Adds a connection between two verticies\
-    Does not do anything if one or both of the verticies do not exist in m already
-    void DFS(int start); // Depth First Search\
-    go through all of the points and print the path
+    void add_edge(int v1, int v2); // Adds a connection between two verticies; Does not do anything if one or both of the verticies do not exist in m already
+    void DFS(int start); // Depth First Search; go through all of the points and print the path
 };
 
 #endif // GRAPH_H
